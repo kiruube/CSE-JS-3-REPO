66e7d7eb6fb436030d41311280df9d9c970b853f
Isaac Kalumba <ikalumba221@gmail.com>
2024-05-02 10:01:21 +0300

    create a connection to the cloud database

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 // 1. Import Dependencies
 const express = require("express");
-
+const mongoose = require("mongoose");
 
 // 2. Import Configurations
-const PORT = 3000;
+
+const { mongoUri, PORT } = require("./config");
 
 // 3. Import Routes:
 
 // 4. Create a Database connection using the mongoose Library
+mongoose
 
+  .connect(mongoUri)
 
+  .then(() => {
+    console.log("Database Connection was successful...");
+  })
+
+  .catch((err) => {
+    console.log(err);
+  });
 // 5. Create a Web server application instance using the Express library
 const app = express();
-
 
 // 6. Set middlewares for the application
 
 // 7. Set entry points for the REST routes created for the different Collections.
-app.get("/", (req,res)=> {
-    res.send("hello cse-js-3");
-})
-
+app.get("/", (req, res) => {
+  res.send("hello cse-js-3");
+});
 
 // 8. Configure the web server application to listen to requests.
-app.listen (PORT, ()=> console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
