f2e14ec59a870a63df3c00f8944fa952fe8dc7cf
senjack <senjackglobal@gmail.com>
2024-05-08 09:48:56 +0300

    Add NavBar

--- views/src/components/NavComponent.vue
+++ views/src/components/NavComponent.vue
@@ -1,6 +1,35 @@
 <template>
     <div>
-        <h4>This is a Navigation Bar</h4>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Courses</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Students</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Staff</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Departments</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Partners</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+        </nav>
     </div>
 </template>
 
--- views/src/views/GeneralView.vue
+++ views/src/views/GeneralView.vue
@@ -1,7 +1,6 @@
 
 <template> 
     <div>
-        <h1>This is a general page</h1>
         <NavComponent></NavComponent>
     </div>
 </template>
