c98919dc7c4d69327876c9f9df96a11ba0f32431
Arnold <arnolduwamz7@gmail.com>
2024-05-09 10:11:52 +0300

    configured routes for all fields

--- views/src/router/index.js
+++ views/src/router/index.js
@@ -12,15 +12,80 @@ const router = createRouter({
       component: StudentsDetailView
     },
     {
+      path: '/students/register',
+      name: 'StudentsDetailView',
+      component: StudentsDetailView
+    },
+    {
+      path: '/students',
+      name: 'StudentsDetailView',
+      component: StudentsDetailView
+    },
+    {
       path: '/students/detail',
       name: 'StudentsDetailView',
       component: StudentsDetailView
     },
     {
+      path: '/departments/register',
+      name: 'DepartmentsView',
+      component: DepartmentsView
+    },
+    {
       path: '/departments',
       name: 'DepartmentsView',
       component: DepartmentsView
-    }
+    },
+    {
+      path: '/departments/detail',
+      name: 'DepartmentsView',
+      component: DepartmentsView
+    },
+    {
+      path: '/courses/register',
+      name: 'DepartmentsView',
+      component: DepartmentsView
+    },
+    {
+      path: '/courses',
+      name: 'DepartmentsView',
+      component: DepartmentsView
+    },
+    {
+      path: '/courses/detail',
+      name: 'DepartmentsView',
+      component: DepartmentsView
+    },
+    {
+      path: '/partners/register',
+      name: 'DepartmentsView',
+      component: DepartmentsView
+    },
+    {
+      path: '/partners',
+      name: 'DepartmentsView',
+      component: DepartmentsView
+    },
+    {
+      path: '/partners/detail',
+      name: 'DepartmentsView',
+      component: DepartmentsView
+    },
+    {
+      path: '/staff/register',
+      name: 'DepartmentsView',
+      component: DepartmentsView
+    },
+    {
+      path: '/staff',
+      name: 'DepartmentsView',
+      component: DepartmentsView
+    },
+    {
+      path: '/staff/detail',
+      name: 'DepartmentsView',
+      component: DepartmentsView
+    }  
   ]
 })
 
