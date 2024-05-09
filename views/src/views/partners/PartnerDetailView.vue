98c75edb8bab0fdb86ecafe218e3f2f7002d1d26
Busingye Lilian <busingyelillian11@gmail.com>
2024-05-09 09:31:15 +0300

    updated the PartnersDetailView

---
+++ views/src/views/partners/PartnerDetailView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>This is a PartnerDetailView</h1>
+    </div>
+</template>
+
+<script>
+export default 
+    {name: "PartnerDetailView"}
+
+</script>
+
+<style>
+
+</style>
