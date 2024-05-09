ac2546de452578edcff85a8efa6400dcf2c2678c
Phillip <phillipbaguma1@gmail.com>
2024-05-09 09:16:55 +0300

    add department list view

---
+++ views/src/views/departments/DepartmentsView.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <h1>This is a Department's List View</h1>
+</div>    
+</template>
+
+
+
+<script>
+export default{name: "DepartmentsView"}
+</script>
+
+
+<style>
+
+</style>
