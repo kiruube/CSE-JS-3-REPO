e8a65a112bdc85d0c0d02280bcd6b62d9f7452e0
Arnold <arnolduwamz7@gmail.com>
2024-05-09 09:16:47 +0300

    add routes

---
+++ views/src/views/students/StudentsDetailView.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <h1>This is a StudentsDetailView</h1>
+</div>
+    
+</template>
+
+
+<script>
+export default {name: "StudentsDetailView"}
+</script>
+
+
+<style >
+
+</style>
